3adbf326c98eeb03569eae08379886a3e27926cf
İbrahim Hakkı Ergin <ibrahimh.ergin@gmail.com>
2025-07-18 11:53:39 +0300

    fix: update backup script to log output and use correct tar command for compression

--- script.sh
+++ script.sh
@@ -10,8 +10,11 @@ year=$(date +%Y)
 time=$(date +%H%M%S)
 
 backup_file_name="backup_$day-$year-$time"
+logfile_name="log_$day-$year-$time"
+
 echo "Backup file : $(pwd)/$backup_file_name.tar.gz"
 
-tar -czf "${backup_file_name}.tar.gz" "$SOURCE_DIR"
+tar -cvf "${backup_file_name}.tar.gz" "$SOURCE_DIR" > "$logfile_name"
 
 echo "Backup finished."
+echo "Logged in $logfile_name"
