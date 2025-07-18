da58abe76f4f9ff6eeb99f6dd3502c20b79f2437
İbrahim Hakkı Ergin <ibrahimh.ergin@gmail.com>
2025-07-18 12:10:38 +0300

    fix: correct timestamp format in backup script and update tar command for compression

--- script.sh
+++ script.sh
@@ -7,14 +7,15 @@ BACKUP_ROOT_DIR=$2
 
 day=$(date +%d)
 year=$(date +%Y)
-time=$(date +%H%M%S)
+time=$(date +%H.%M.%S)
 
 backup_file_name="backup_$day-$year-$time"
 logfile_name="log_$day-$year-$time"
 
 echo "Backup file : $(pwd)/$backup_file_name.tar.gz"
 
-tar -cvf "${backup_file_name}.tar.gz" "$SOURCE_DIR" > "$logfile_name"
+tar -czf "${backup_file_name}.tar.gz" "$SOURCE_DIR"
+tar -tvzf "${backup_file_name}.tar.gz" > "$logfile_name"
 
 echo "Backup finished."
 echo "Logged in $logfile_name"
